5e67cf813ad02c28f2a0a8b2f75a228f1fc10f94
AKSHIT KUMAR <akskumar3@publicisgroupe.net>
2021-11-22 10:16:03 +0530

    Added delete mutation

--- GraphQL/graphql-basics/src/index.js
+++ GraphQL/graphql-basics/src/index.js
@@ -75,8 +75,33 @@ const typeDefs = `
     }
 
     type Mutation {
-        createUser(name: String! , email: String!,  age: Int): User!
-        createPost(title: String! , body: String!, published: Boolean!,author: ID!): Post!
+   
+        createUser(data: CreateUserInput!): User!
+        deleteUser(id: ID!): User!
+        createPost(data: CreatePostInput!): Post!
+        deletePost(id: ID!): Post!
+        createComment(data: CreateCommentInput!): Comment!
+        deleteComment(id: ID!): Comment!
+    }
+
+
+    input CreateUserInput {
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    input CreatePostInput {
+        title: String!
+        body: String!
+        published: Boolean!
+        author: ID!
+    }
+
+    input CreateCommentInput {
+        text: String!
+        author: ID!
+        post: ID!
     }
 
     type User {
@@ -169,7 +194,87 @@ const resolvers = {
 
         // }
 
+
+    deleteUser(parent, args, ctx, info) {
+        const userIndex = users.findIndex((user) => user.id === args.id)
+
+        if (userIndex === -1) {
+            throw new Error('User not found')
+        }
+
+        const deletedUsers = users.splice(userIndex, 1)
+
+        // posts = posts.filter((post) => {
+        //     const match = post.author === args.id
+
+        //     if (match) {
+        //         comments = comments.filter((comment) => comment.post !== post.id)
+        //     }
+
+        //     return !match
+        // })
+        // comments = comments.filter((comment) => comment.author !== args.id)
+
+        return deletedUsers[0]
     },
+    createPost(parent, args, ctx, info) {
+        const userExists = users.some((user) => user.id === args.data.author)
+
+        if (!userExists) {
+            throw new Error('User not found')
+        }
+
+        const post = {
+            id: uuidv4(),
+            ...args.data
+        }
+
+        posts.push(post)
+
+        return post
+    },
+    deletePost(parent, args, ctx, info) {
+        const postIndex = posts.findIndex((post) => post.id === args.id)
+        
+        if (postIndex === -1) {
+            throw new Error('Post not found')
+        }
+
+        const deletedPosts = posts.splice(postIndex, 1)
+
+        // comments = comments.filter((comment) => comment.post !== args.id)
+
+        return deletedPosts[0]
+    },
+    createComment(parent, args, ctx, info){
+        const userExists = users.some((user) => user.id === args.data.author)
+        const postExists = posts.some((post) => post.id === args.data.post && post.published)
+
+        if (!userExists || !postExists) {
+            throw new Error('Unable to find user and post')
+        }
+
+        const comment = {
+            id: uuidv4(),
+            ...args.data
+        }
+
+        comments.push(comment)
+
+        return comment
+    },
+    deleteComment(parent, args, ctx, info) {
+        const commentIndex = comments.findIndex((comment) => comment.id === args.id)
+
+        if (commentIndex === -1) {
+            throw new Error('Comment not found')
+        }
+
+        const deletedComments = comments.splice(commentIndex, 1)
+
+        return deletedComments[0]
+    }
+},
     Post: {
         author(parent, args, ctx, info) {
             return users.find((user) => {
